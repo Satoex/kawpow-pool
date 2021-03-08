1c9831009f235917e440b9f2b7e01c3fb43a73a2
aymoon05 <79243339+aymoon05@users.noreply.github.com>
2021-03-08 08:54:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,10 @@ sleep 3
 
 sudo rm -rf /usr/lib/node_modules
 sudo apt remove --purge -y nodejs node
-sudo rm /etc/apt/sources.list.d/nodesource.list*
+sudo rm /etc/apt/sources.list.d/*
 sudo apt update
 sudo apt upgrade -y
+sudo apt-get dist-upgrade -y
 sudo apt install -y software-properties-common build-essential autoconf pkg-config make gcc g++ screen nano wget curl ntp fail2ban 
 
 sudo add-apt-repository -y ppa:chris-lea/redis-server
@@ -20,6 +21,7 @@ sudo add-apt-repository -y ppa:bitcoin/bitcoin
 
 sudo apt update
 sudo apt install -y libdb4.8-dev libdb4.8++-dev libssl-dev libboost-all-dev libminiupnpc-dev libtool autotools-dev redis-server
+sudo apt-get install -y sudo git npm nodejs nodejs-legacy      
 
 sudo systemctl enable fail2ban
 sudo systemctl start fail2ban
@@ -30,18 +32,19 @@ sudo systemctl start ntp
 
 sudo rm -rf ~/.nvm
 sudo rm -rf ~/.npm
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | sudo bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 source ~/.bashrc
 sudo chown -R $USER:$GROUP ~/.nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install v8.1.4
 nvm use v8.1.4
 npm update -g
 
 npm install -g webpack@4.29.3
 npm install -g pm2@4.2.1
-npm install -g npm@latest
+npm install -g npm@latest-6
 
 npm install
 npm update
